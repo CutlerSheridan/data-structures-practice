011721042366b72d5866cf9cca1938a6ab3f3b4c
Cutler Sheridan <cutler.sheridan@gmail.com>
2022-08-25 13:17:30 -0700

    In BNS, add delete, find, inorder, preorder, postorder, levelorder

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -1,13 +1,31 @@
 const Tree = (valArray) => {
+  const mergeSort = (array) => {
+    if (array.length <= 1) {
+      return array;
+    }
+    let rightArr = mergeSort(array.splice(Math.floor(array.length / 2)));
+    let leftArr = mergeSort(array);
+    const newArr = [];
+    do {
+      if (leftArr[0] === rightArr[0]) {
+        newArr.push(leftArr.shift(), rightArr.shift());
+      } else if (leftArr.length > 0 && rightArr.length > 0) {
+        newArr.push(leftArr[0] < rightArr[0] ? leftArr.shift() : rightArr.shift());
+      } else {
+        newArr.push(leftArr.length > 0 ? leftArr.shift() : rightArr.shift());
+      }
+    } while (leftArr.length > 0 || rightArr.length > 0);
+    return newArr;
+  };
   const insert = (value, node = root) => {
     if (value < node.data) {
-      if (node.left === null) {
+      if (!node.left) {
         node.left = Node(value);
       } else {
         insert(value, node.left);
       }
     } else if (value > node.data) {
-      if (node.right === null) {
+      if (!node.right) {
         node.right = Node(value);
       } else {
         insert(value, node.right);
@@ -20,27 +38,82 @@ const Tree = (valArray) => {
     } else if (value > node.data) {
       node.right = deleteNode(value, node.right);
     } else if (value === node.data) {
-      if (node.left === null) {
+      if (!node.left) {
         return node.right;
-      } else if (node.right === null) {
+      } else if (!node.right) {
         return node.left;
       } else {
-        // find inorder successor and assign that node to the current node, then call delete on the right branch of the tree
+        const orderedArr = inorder(null, node);
+        const currentIndex = orderedArr.indexOf(value);
+        const replacementNode = find(orderedArr[currentIndex + 1]);
+        node.data = replacementNode.data;
+        node.right = deleteNode(replacementNode.data, node.right);
       }
     }
+    return node;
   };
-  // const buildTree = (arr) => {
-  //   if (arr.length === 1) {
-  //     return Node(arr[0]);
-  //   }
-  //   const newRoot = Node(arr.shift());
-  //   while (arr.length > 0) {
-  //     const nextNum = arr.shift();
-  //     insert(nextNum, newRoot);
-  //   }
-  //   return newRoot;
-  // };
-  const buildBalancedTree = (arr) => {
+  const find = (value, node = root) => {
+    if (node.data === value) {
+      return node;
+    }
+    const direction = value < node.data ? 'left' : 'right';
+    if (!node[direction]) {
+      return null;
+    }
+    return find(value, node[direction]);
+  };
+  const levelOrder = (func, nodeArr = [root]) => {
+    const current = nodeArr.shift();
+    if (current.left) {
+      nodeArr.push(current.left);
+    }
+    if (current.right) {
+      nodeArr.push(current.right);
+    }
+    if (!func) {
+      return nodeArr.length === 0
+        ? [current.data]
+        : [current.data].concat(levelOrder(func, nodeArr));
+    }
+    func(current);
+    if (nodeArr.length !== 0) {
+      levelOrder(func, nodeArr);
+    }
+  };
+  const inorder = (func, node = root) => {
+    if (!node) {
+      return [];
+    }
+    if (!func) {
+      return inorder(func, node.left).concat(node.data).concat(inorder(func, node.right));
+    }
+    inorder(func, node.left);
+    func(node);
+    inorder(func, node.right);
+  };
+  const preorder = (func, node = root) => {
+    if (!node) {
+      return [];
+    }
+    if (!func) {
+      return preorder(func, node.left).concat(preorder(func, node.right)).concat(node.data);
+    }
+    preorder(func, node.left);
+    preorder(func, node.right);
+    func(node);
+  };
+  const postorder = (func, node = root) => {
+    if (!node) {
+      return [];
+    }
+    if (!func) {
+      return [node.data].concat(postorder(func, node.left)).concat(postorder(func, node.right));
+    }
+    func(node);
+    postorder(func, node.left);
+    postorder(func, node.right);
+  };
+  const buildTree = (arr) => {
     const arrSize = arr.length;
     if (arrSize === 1) {
       return Node(arr[0]);
@@ -51,16 +124,15 @@ const Tree = (valArray) => {
     arr.splice(middleLoc);
     const leftArr = arr;
     if (middleLoc > 0) {
-      newNode.left = buildBalancedTree(leftArr);
+      newNode.left = buildTree(leftArr);
     }
     if (middleLoc < arrSize - 1) {
-      newNode.right = buildBalancedTree(rightArr);
+      newNode.right = buildTree(rightArr);
     }
     return newNode;
   };
-  // const root = buildTree(valArray);
   // i should change this to a merge sort
-  const root = buildBalancedTree(valArray.sort((a, b) => a - b));
+  const root = buildTree(mergeSort(valArray));
   const prettyPrint = (node = root, prefix = '', isLeft = true) => {
     if (node.right !== null) {
       prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
@@ -70,7 +142,7 @@ const Tree = (valArray) => {
       prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
   };
-  return { root, insert, deleteNode, prettyPrint };
+  return { root, insert, deleteNode, find, levelOrder, inorder, preorder, postorder, prettyPrint };
 };
 
 const Node = (data = null, left = null, right = null) => {
@@ -81,3 +153,16 @@ const tree = Tree([5, 6, 2, 9, 1, 3, 16, 17, 18, 15]);
 tree.insert(10);
 tree.insert(4);
 tree.prettyPrint();
+tree.deleteNode(6);
+tree.prettyPrint();
+// tree.levelOrder((node) => console.log(node.data * -1));
+// console.log(tree.levelOrder());
+// console.log('find 5 node:');
+// console.log(tree.find(5));
+// console.log('find nonexistant node:');
+// console.log(tree.find(2000));
+// console.log([].concat([5, 6]));
+console.log(tree.inorder());
+// console.log(tree.inorder((node) => console.log(node.data * -1)));
+console.log(tree.preorder());
+console.log(tree.postorder());
